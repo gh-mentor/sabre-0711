043c462a1a2de12127738bd1a36bb22d8ddc94cf
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-07-11 12:28:11 -0400

    Update

---
+++ push.sh
@@ -0,0 +1,12 @@
+# This file contains git commands to stage, commit, and push changes to the 'main' branch of the remote repository.
+
+# Stage all changes using the 'stage' command
+git stage .
+
+# Commit all changes using the 'commit' command
+git commit -m "Update"
+
+# Push changes to the 'main' branch of the remote repository using the 'push' command
+git push origin main
+
+# End of script
